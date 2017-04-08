b43ffc912a1d28dad665063d7cbf21c63e03a408
Venkatesh Katabathula <WS33@global.tesco.org>
2017-04-08 11:57:55 +0530

    Updated PageNotFound to look a bit good

--- src/components/notFound.js
+++ src/components/notFound.js
@@ -5,13 +5,18 @@
 var React = require('react');
 var Link = require('react-router').Link;
 
-class PageNotFound extends React.Component{
-  render(){
-    return(
+class PageNotFound extends React.Component {
+  render () {
+    return (
       <div>
         <h2>Oops! Page Not Found</h2>
-        <h4>{this.props.location.pathname} is not found</h4>
-        <h5>The Page would be under maintenance or coming soon.</h5>
+        <h4>The URL you have requested for "{this.props.location.pathname}" is not found</h4>
+
+        <h5>The possible reasons would be as follows:</h5>
+        <ul>
+          <li><h5>The page does not exist</h5></li>
+          <li><h5>The Page would be under maintenance or coming soon.</h5></li>
+        </ul>
         <h5>Thanks for your patience.</h5>
         <p>Click here to go <Link to="/">Back to Home</Link></p>
       </div>
